4b5e3bc3efa42a3cb0966a7fb77f095a528744e1
agarciamog <agarciamog@gmail.com>
2017-08-30 12:01:30 -0600

    Added CreateAndPopulateZip Class.

--- src/io/albertogarcia/workshop/createandpopulatezip/Main.java
+++ src/io/albertogarcia/workshop/createandpopulatezip/Main.java
@@ -1,8 +1,19 @@
 package io.albertogarcia.workshop.createandpopulatezip;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CreateAndPopulateZip obj = new CreateAndPopulateZip("myData.zip");
+        try {
+            FileSystem zipFs = obj.openZip();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
